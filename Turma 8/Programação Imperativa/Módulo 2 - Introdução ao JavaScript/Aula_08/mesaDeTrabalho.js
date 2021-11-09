1be4db3a2e2d06c46594f7cc205638ca038e6f1c
Douglas <douglassnascimet@gmail.com>
2021-11-08 22:45:43 -0300

    Enviando a mesa de trabalho de Front-End I

--- Turma 8/Programação Imperativa/Módulo 2 - Introdução ao JavaScript/Aula_08/mesaDeTrabalho.js
+++ Turma 8/Programação Imperativa/Módulo 2 - Introdução ao JavaScript/Aula_08/mesaDeTrabalho.js
@@ -1,44 +1,37 @@
 // 1
 
-
-function podeIr(altura,acompanhante){
-    if ((altura > 1.40) && (altura < 2)) {
-        return true;
-    } else if(altura < 1.40  && (altura > 1.2)){
-        return acompanhante;
-    } else if(altura <= 1.20 || altura > 2) {
-        return 'Acesso negado !'
-    }
-};
+function podeIr(altura, acompanhante) {
+  if (altura > 1.4 && altura < 2) {
+    return true
+  } else if (altura < 1.4 && altura > 1.2) {
+    return acompanhante
+  } else if (altura <= 1.2 || altura > 2) {
+    return 'Acesso negado !'
+  }
+}
 
 console.log()
 console.log(podeIr(1.41))
 console.log()
-console.log(podeIr(1.39,true))
+console.log(podeIr(1.39, false))
 console.log()
 console.log(podeIr(2.1))
 console.log()
 console.log(podeIr(1.19))
 console.log()
-console.log()
-console.log()
-
-
 
 // 2
 
-function podeSubir(altura,acompanhante){
-    if ((altura > 1.40) && (altura < 2)) {
-        return 'Acesso autorizado !'
-    } else if((altura < 1.40) && (altura > 1.2)){
-        return 'Acesso autorizado somente com acompanhante.'
-    } else if((altura < 1.20) || (altura > 2)) {
-        return 'Acesso negado !'
-    }
-};
+function podeSubir(altura) {
+  if (altura > 1.4 && altura < 2) {
+    return 'Acesso autorizado !'
+  } else if (altura < 1.4 && altura > 1.2) {
+    return 'Acesso autorizado somente com acompanhante.'
+  } else if (altura < 1.2 || altura > 2) {
+    return 'Acesso negado !'
+  }
+}
 
-
-console.log()
 console.log(podeSubir(1.41))
 console.log('Maior que 1.40 metros')
 console.log()
