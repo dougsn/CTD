a041c27690ea4f3f5bec5e8aa7c0111c9209ce98
Douglas da Silva Nascimento <dnascimento@enavirj.com.br>
2022-03-15 15:53:49 -0300

    Formatando a parte de estilos via JS e CSS do página e fazendo a lógica do backgroundImage e afins

--- Turma 6/Front-End 2/Módulo 3 [Eventos]/A8A/main.js
+++ Turma 6/Front-End 2/Módulo 3 [Eventos]/A8A/main.js
@@ -1,15 +1,21 @@
 let body = document.querySelector('body')
+body.style.background = '#CCCCCC'
 
-let h1 = document.createElement('h1')
-h1.innerText = "Formulário"
+let legend = document.createElement('legend')
+legend.innerText = "Formulário"
+legend.style.color = "#FFF"
+legend.style.fontSize = "28px"
+legend.style.fontWeight = "500"
 
-h1.onmouseover = () => {
-    h1.style.color = "red"
+legend.onmouseover = () => {
+    legend.style.color = "#333"
+    legend.style.transition = '.5s'
 }
 
-h1.onmouseout = () => {
-    h1.style.color = "blue"
+legend.onmouseout = () => {
+    legend.style.color = "#FFF"
 }
+
 
 let p = document.createElement('p')
 let p2 = document.createElement('p')
@@ -27,29 +33,29 @@ divBtn.setAttribute("class", "btn")
 // @@ Cx 1
 
 let label1 = document.createElement('label');
-label1.innerText = "Nome:";
-label1.setAttribute("for", "nome")
+label1.innerText = "URL para mudar o background da página:";
+label1.setAttribute("for", "body")
 
 let input1 = document.createElement('input');
-input1.setAttribute("name", "nome")
-input1.setAttribute("id", "nome")
+input1.setAttribute("name", "body")
+input1.setAttribute("id", "body")
 
 
 // @@ Cx 2
 
 let input2 = document.createElement('input');
-input2.setAttribute("for", "sobrenome")
-input2.setAttribute("id", "sobrenome")
+input2.setAttribute("for", "nome")
+input2.setAttribute("id", "nome")
 
 let label2 = document.createElement('label');
-label2.innerText = "Sobrenome:";
-label2.setAttribute("for", "sobrenome")
+label2.innerText = "Nome:";
+label2.setAttribute("for", "nome")
 
 // @@ Botões
 
 let btnSubmite = document.createElement('input')
 btnSubmite.setAttribute("type", "submit")
-btnSubmite.setAttribute("value", "Enviar")
+btnSubmite.setAttribute("value", "Trocar")
 btnSubmite.setAttribute("class", "btnEnviar")
 
 btnSubmite.addEventListener('click', (evento) => {
@@ -62,8 +68,9 @@ btnReset.setAttribute("value", "Limpar")
 
 
 body.appendChild(section)
-section.appendChild(h1)
+// section.appendChild(h1)
 section.appendChild(form)
+form.appendChild(legend)
 form.appendChild(divNome)
 divNome.appendChild(label1)
 divNome.appendChild(input1)
@@ -76,21 +83,51 @@ divBtn.appendChild(btnReset)
 // section.appendChild(p)
 
 window.onload = () => {
-    alert("A página foi carregada !")
+    // alert("A página foi carregada !")
 } 
 
-      
-input1.onkeyup = () => {
+
+
+
+
+
+// Realizando a adição dos eventos
+
+
     
+input1.addEventListener('keyup', () => {
     let valorInput = input1.value
     let valorNome = input1.value.length;
     section.appendChild(p)
     section.appendChild(p2)
     p.innerText = valorNome
     p2.innerText = valorInput
-    
-}
-    
+})
 
 
+btnSubmite.addEventListener('click', () => {
+    let container = document.querySelector('.container')
+    let valorInput = input1.value
+    console.log(valorInput);
 
+    body.style.backgroundImage = `url(${valorInput})`
+    body.style.backgroundRepeat = 'no-repeat'
+    body.style.backgroundSize = 'cover'
+
+    if(body.style.backgroundImage != 'none'){
+        container.style.color = 'white';
+    }
+
+})
+
+btnReset.addEventListener('click', () => {
+
+    let valorInput = input2.value
+
+    form.style.backgroundImage = `url(${valorInput})`
+    form.style.backgroundRepeat = 'no-repeat'
+    form.style.backgroundSize = 'cover'
+    form.style.boxShadow = '2px 2px 55px 6px rgba(0,0,0,0.24);'
+
+
+})
