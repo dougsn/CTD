601f7de58cea3aa662166076fc34dd76632340a6
Douglas da Silva Nascimento <dnascimento@enavirj.com.br>
2022-03-31 08:19:15 -0300

    Finalizando a mesa do consumo da API randomUser

--- Turma 6/Front-End 2/Módulo 4 [Validação do lado do cliente]/A17A [API's]/Mesa/js/index.js
+++ Turma 6/Front-End 2/Módulo 4 [Validação do lado do cliente]/A17A [API's]/Mesa/js/index.js
@@ -1,25 +1,37 @@
-// Aqui realizamos a consulta da promisse, esperando sua resposta assíncrona
-fetch('https://randomuser.me/api/')
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        //manipulamos a resposta
-        console.log(data.results[0])
-    });
-
 function renderizarDadosUsuario(dados) {
-    /* -------------------------------- Tarefa 1 -------------------------------- */
-    // Aqui devem desenvolver uma função que seja exibida na tela:
-    // a foto, o nome completo do usuário e o e-mail.
-    // Isto deve ser baseado nas informações que obtemos da API e inseridas no HTML.
+  /* -------------------------------- Tarefa 1 -------------------------------- */
+  // Aqui devem desenvolver uma função que seja exibida na tela:
+  // a foto, o nome completo do usuário e o e-mail.
+  // Isto deve ser baseado nas informações que obtemos da API e inseridas no HTML.
 
-    let card = document.querySelector('.card')
+  let card = document.querySelector(".card");
 
+  card.innerHTML = `
+          <img src="${dados.picture.large}" alt="">
+          <h1>${dados.name.title} ${dados.name.first} ${dados.name.last}</h1>
+          <p>${dados.email}</p>
+      `;
 }
-
 
 /* --------------------------- Tarefa 2 (extra) --------------------------- */
 // Aqui você pode ir para o ponto extra de usar o botão que está comentado no HTML.
 // Você pode descomentar o código no index.html e usar esse botão para executar uma nova solicitação API, sem recarregar a página.
 // Cabe aos desenvolvedores decidirem qual bloco de código deve ser contido dentro de uma função para que ele possa ser executado toda vez que um clique de botão for realizado.
+
+// Aqui realizamos a consulta da promisse, esperando sua resposta assíncrona
+
+let randomButton = document.getElementById("random");
+
+randomButton.addEventListener("click", () => {
+  fetch("https://randomuser.me/api/")
+    .then((response) => {
+      return response.json(); // Convertendo o JSON para JavaScript
+    })
+    .then((result) => {
+      //manipulamos a resposta
+      return result.results[0]; // Separando a parte que eu quero manipular (que é o primeiro elemento do array)
+    })
+    .then((render) => {
+      return renderizarDadosUsuario(render); // Renderizando na página quando clicar no botão, que aciona a função criada.
+    });
+});
