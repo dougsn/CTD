3a16f4f877892d5e013585307150d50a6d4871b4
dougsn <douglassnascimet@gmail.com>
2022-06-20 22:37:13 -0300

    Finalizando os testes unitarios da calculadora

--- Turma 6/Testing l/Módulo 6 [Teste Unitário]/A15S [Teste Unitário]/Calc/__test__/soma.calc.test.js
+++ Turma 6/Testing l/Módulo 6 [Teste Unitário]/A15S [Teste Unitário]/Calc/__test__/soma.calc.test.js
@@ -7,5 +7,17 @@ describe('Testes de soma', () => {
         expect(soma(2,2)).toBe(4);
     })
 
+    test('Soma de dois zeros', () => { 
+        expect(soma(0,0)).toBe(0);
+    })
+
+    test('Soma de dois nulos', () => { 
+        expect(soma(null,null)).toBe(0);
+    })
+
+    test('Soma de string', () => { 
+        expect(soma('a','b')).toBe('ab');
+    })
+
 })
 
--- Turma 6/Testing l/Módulo 6 [Teste Unitário]/A15S [Teste Unitário]/Calc/__test__/subtracao.calc.test.js
+++ Turma 6/Testing l/Módulo 6 [Teste Unitário]/A15S [Teste Unitário]/Calc/__test__/subtracao.calc.test.js
@@ -7,5 +7,21 @@ describe('Testes de subtracao', () => {
         expect(subtracao(4,2)).toBe(2);
     })
 
+    test('Subtracao de dois zeros', () => { 
+        expect(subtracao(0,0)).toBe(0);
+    })
+
+    test('Subtracao de strings', () => { 
+        expect(subtracao('a', 'b')).toBe(NaN);
+    })
+
+    test('Subtracao de numeros negativo com positivo', () => { 
+        expect(subtracao(-5, 5)).toBe(-10);
+    })
+
+    test('Subtracao de numeros negativos', () => { 
+        expect(subtracao(-5, -5)).toBe(0);
+    })
+
 })
 
