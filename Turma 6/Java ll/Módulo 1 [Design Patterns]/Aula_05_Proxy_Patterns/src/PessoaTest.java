6dee74704aca4599885a56685f3afa6632abf49f
dougsn <douglassnascimet@gmail.com>
2022-08-03 22:38:34 -0300

    Att com ProxyPatterns

---
+++ Turma 6/Java ll/Módulo 1 [Design Patterns]/Aula_05_Proxy_Patterns/src/PessoaTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.Test;
+
+import java.text.DateFormat;
+import java.text.FieldPosition;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class PessoaTest {
+
+    Date data = new Date();
+
+    @Test
+    public void vacinar1(){
+        Pessoa p1 = new Pessoa("Juca","Silva","554432123",data,"Pfizer");
+        Vacinar vacinar = new ServicoVacinarProxy();
+
+        vacinar.vacinarPessoa(p1.getRg(), p1.getDataVacina(),p1.getNomeVacina());
+        System.out.println(p1);
+
+
+    }
+
+
+
+}
---
+++ Turma 6/Java ll/Módulo 1 [Design Patterns]/Aula_05_Proxy_Patterns/src/ServicoVacinar.java
@@ -0,0 +1,9 @@
+import java.util.Date;
+
+public class ServicoVacinar implements Vacinar{
+
+    @Override
+    public void vacinarPessoa(String rg, Date dataVacina, String tipoVacina) {
+        System.out.println("Usuário(a) " + rg + " vacinado(a) em " + dataVacina + " com a vacina " + tipoVacina + " com sucesso");
+    }
+}
