6850982b8e5507046aa12743d2de991b3c614dfc
dougsn <douglassnascimet@gmail.com>
2022-10-31 20:53:07 -0300

    Finalizando aula de useState()

--- Turma 6/Front-End 3/Módulo 2 [Componentização em React]/Aula 7 [Hooks]/mesa_hook/src/App.jsx
+++ Turma 6/Front-End 3/Módulo 2 [Componentização em React]/Aula 7 [Hooks]/mesa_hook/src/App.jsx
@@ -2,23 +2,24 @@ import { useState } from "react";
 
 const App = () => {
 
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState([1]);
 
 
-  function addItem(){
+  const addItem = () => {
     setItems([...items, items.length + 1]); // Pegando o último elemento do array e somando mais 1 no items.
   }
 
   return (
     <>
+    
       <ul>
         {
-          items.map((item) => {
-            <li>{`O item ${item} foi adicionado à lista!`}</li>
-          })
+          items.map((val) => (
+            <li>{`O item ${val} foi adicionado à lista!`}</li>
+          ))
         }
       </ul>
-      <button onClick={addItem}>Aperte</button>
+      <button onClick={addItem}>Adicione itens na lista</button>
     </>
   )
 
