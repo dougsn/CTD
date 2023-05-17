0a84572690b95f1b4d77967dbdf2872799becf28
Douglas Nascimento <douglassnascimet@gmail.com>
2023-05-17 15:13:42 -0300

    Realizando a mesa de trabalho de 16_05

---
+++ Especializacao/Especializacao Back-end/Módulo 2 - Framework Spring Cloud/A5A - Configuracao em Sistemas Distribuidos/Mesa/client/src/test/java/com/dh/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dh.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
