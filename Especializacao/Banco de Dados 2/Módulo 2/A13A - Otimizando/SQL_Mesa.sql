d51bd68f88f8f330dc8e7795c616c4d9cd134392
Douglas Nascimento <douglassnascimet@gmail.com>
2023-06-05 10:36:34 -0300

    Comecando a mesa de trabalho de Indices

---
+++ Especializacao/Banco de Dados 2/Módulo 2/A13A - Otimizando/SQL_Mesa.sql
@@ -0,0 +1,61 @@
+## Verificar que tabela é a modelo.
+
+SELECT * FROM adventureworks.productdescription;
+
+-- 1 - Crie um índice FULLTEXT na tabela productdescription, utilizando como parâmetro a coluna `description`.
+create fulltext index indice_description on adventureworks.productdescription (description);
+
+-- 2 - Crie uma consulta que retorne o nome do modelo e a quantidade de vezes que a descrição 
+-- “Suitable for any type of riding, on or off-road” aparece em cada modelo.
+
+select * from adventureworks.productdescription
+where match (description)
+against ('"Suitable for any type of riding, on or off-road"' in boolean mode);
+
+## Quantos registros retornaram?
+	-- Retornou 1 registro.
+
+## Qual o tempo de duração dessa consulta?
+    -- 0 segundos
+
+## Em quais modelos a descrição citada aparece mais de uma vez?
+	-- Só em 1 *
+
+
+## 3 - Crie uma consulta que retorne a porcentagem de relevância da descrição
+--  “Suitable for any type of riding, on or off-road.”  para todos os modelos envolvidos.
+
+
+## 4 - Visualize todos os índices da tabela productdescription
+
+## Em qual coluna está o índice clusterizado?
+	-- Está na coluna ProductDescriptionID
+    
+## Em qual coluna está o índice não clusterizado?
+	-- Está na coluna Description
+    
+## Qual o tipo de estrutura do índice clusterizado?
+	-- BTREE
+## Qual o tipo de estrutura do índice não clusterizado?
+	-- FULLTEXT
+
+## 5 - Acesse o  Schema Inspector do banco de dados AdventureWorks e responda:
+
+## Qual o tamanho do banco de dados?
+	-- 72.2MiB
+## Qual o tamanho da maior tabela desse banco de dados?
+	-- 11.5MiB
+
+##  Acesse o Table Inspector da tabela productdescription e responda:
+
+## Qual o tamanho estimado da tabela?
+	-- 144.0KiB
+    
+## 7 - Selecione a guia Indexes e clique no índice  FULLTEXT. Responda:
+
+## Qual a cardinalidade do índice criado?
+	-- 762
+    
+## Justifique esse valor.
+	-- Indica o número de valores diferente contidos nessa coluna.
+    
